f34d3e2d42c7c6480ff7d1f0c87da2c30c7537ca
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2026-08-20 18:48:51 +0200

    reuse

--- run-gtk.sh
+++ run-gtk.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2026 Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
 
 cargo build
 SHEET=target/debug/sheet examples/sample.sh /tmp/demo
